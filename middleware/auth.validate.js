606f2bc8fa1f3638c5063f2a3a040c10eac05c1e
Phong <phongduytran7999@gmail.com>
2021-04-28 22:12:05 +0700

    fixed: login merchant (auth.validate)

--- middleware/auth.validate.js
+++ middleware/auth.validate.js
@@ -3,6 +3,14 @@ Joi.objectId = require("joi-objectid")(Joi);
 
 const loginValidation = (data) => {
   const schema = Joi.object({
+    username: Joi.string().min(2).max(30).required(),
+    password: Joi.string().required(),
+  });
+  return schema.validate(data);
+};
+
+const loginValidationMerchant = (data) => {
+  const schema = Joi.object({
     email: Joi.string().min(2).max(30).required(),
     password: Joi.string().required(),
   });
@@ -11,4 +19,5 @@ const loginValidation = (data) => {
 
 module.exports = {
   loginValidation,
+  loginValidationMerchant,
 };
